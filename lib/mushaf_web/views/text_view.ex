03845f3e9197bf0bf58597dc3b6a0606277f6446
Mohamed Aly <mksaga@hey.com>
2022-03-31 13:02:16 -0400

    Attempt to separate basmalah (not working)

--- lib/mushaf_web/views/text_view.ex
+++ lib/mushaf_web/views/text_view.ex
@@ -9,6 +9,16 @@ defmodule MushafWeb.TextView do
 
   def get_ayah_text(ayah) do
     elem(ayah, 2)
+    ayah_text = elem(ayah, 2)
+    ayah_number = elem(ayah, 1)
+    surah_number = elem(ayah, 0)
+    if ayah_number == 1 and surah_number != 1 do
+      {basmalah, first_ayah} = String.split_at(ayah_text, 23)
+      basmalah <> "\n" <> first_ayah
+    else
+      ayah_text
+    end
+  end
   end
 
   def get_prev_page_no(page_no) do
