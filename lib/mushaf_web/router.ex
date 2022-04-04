0f63fcadf7ea12f4075e610e9b5f03442d180b23
Mohamed Aly <mksaga@hey.com>
2022-04-04 13:15:56 -0400

    Move up mailbox route

--- lib/mushaf_web/router.ex
+++ lib/mushaf_web/router.ex
@@ -50,6 +50,18 @@ defmodule MushafWeb.Router do
     post "/users/confirm/:token", UserConfirmationController, :update
   end
 
+  # Enables the Swoosh mailbox preview in development.
+  #
+  # Note that preview only shows emails that were sent by the same
+  # node running the Phoenix server.
+  if Mix.env() == :dev do
+    scope "/dev" do
+      pipe_through :browser
+
+      forward "/mailbox", Plug.Swoosh.MailboxPreview
+    end
+  end
+
   scope "/", MushafWeb do
     pipe_through :browser
 
@@ -78,18 +90,6 @@ defmodule MushafWeb.Router do
       pipe_through :browser
 
       live_dashboard "/dashboard", metrics: MushafWeb.Telemetry
-    end
-  end
-
-  # Enables the Swoosh mailbox preview in development.
-  #
-  # Note that preview only shows emails that were sent by the same
-  # node running the Phoenix server.
-  if Mix.env() == :dev do
-    scope "/dev" do
-      pipe_through :browser
-
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
   end
 end
