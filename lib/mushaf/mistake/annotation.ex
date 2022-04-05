fb3b1c06b68b878792376896ef74b446688624f1
Mohamed Aly <mksaga@hey.com>
2022-04-05 13:48:48 -0400

    Assign mistake to user

--- lib/mushaf/mistake/annotation.ex
+++ lib/mushaf/mistake/annotation.ex
@@ -1,10 +1,12 @@
 defmodule Mushaf.Annotation do
   alias Mushaf.Repo
   alias Mushaf.Mistake
+  alias Mushaf.Accounts
 
-  def create_mistake(attrs \\ %{}) do
+  def create_mistake(%Accounts.User{} = user, attrs \\ %{}) do
     %Mistake{}
     |> Mistake.mark_changeset(attrs)
+    |> Ecto.Changeset.put_assoc(:reader, user)
     |> Repo.insert()
   end
 end
