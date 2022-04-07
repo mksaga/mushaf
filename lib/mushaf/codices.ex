1c6a60687b839dc422636827c956b9ced8009cf3
Mohamed Aly <mksaga@hey.com>
2022-04-07 14:50:08 -0400

    Add grant_codex_permission to codices context

--- lib/mushaf/codices.ex
+++ lib/mushaf/codices.ex
@@ -6,13 +6,22 @@ defmodule Mushaf.Codices do
   alias Mushaf.Accounts
 
   def get_codices_by_user(user) do
-    Repo.get_by(Codex, user: user)
+    Repo.all(Codex, user_id: user.id, preload: [:users_with_access])
   end
+
+  def get_codex!(id), do: Repo.get!(Codex, id)
 
   def create_codex(%Accounts.User{} = user, attrs) do
     %Codex{}
     |> Codex.create_changeset(attrs)
     |> Ecto.Changeset.put_assoc(:user, user)
     |> Repo.insert()
+  end
+
+  def grant_codex_permission(codex, user_id, permission) do
+    codex
+    |> Ecto.Changeset.change()
+    |> Ecto.Changeset.put_assoc(:users_with_access, [Accounts.get_user!(user_id), permission])
+    |> Repo.update!()
   end
 end
