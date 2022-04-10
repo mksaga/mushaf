d7f365f5ec2a08bce18b5df78db15ee176059dd8
Mohamed Aly <mksaga@hey.com>
2022-04-10 14:15:59 -0400

    Add some templates, view file, migration

---
+++ lib/mushaf_web/views/codex_view.ex
@@ -0,0 +1,44 @@
+defmodule MushafWeb.CodexView do
+  use MushafWeb, :view
+  alias Mushaf.Text
+
+  def get_ayah_id(ayah) do
+    surah_no = elem(ayah, 0)
+    ayah_no = elem(ayah, 1)
+    "#{surah_no}-#{ayah_no}"
+  end
+
+  def is_first_ayah(ayah) do
+    elem(ayah, 1) == 1
+  end
+
+  def surah_title(ayah) do
+    surah_no = elem(ayah, 0)
+    "سورة " <> Text.get_surah_name(surah_no)
+  end
+
+  def surah_starts_with_basmalah(ayah) do
+    surah_no = elem(ayah, 0)
+    surah_no != 9 and surah_no != 1
+  end
+
+  def get_ayah_text(ayah) do
+    ayah_text = elem(ayah, 2)
+    ayah_number = elem(ayah, 1)
+    surah_number = elem(ayah, 0)
+    if ayah_number == 1 and surah_number != 1 do
+      {basmalah, first_ayah} = String.split_at(ayah_text, 23)
+      first_ayah
+    else
+      ayah_text
+    end
+  end
+
+  def get_prev_page_no(page_no) do
+    max(1, page_no - 1)
+  end
+
+  def get_next_page_no(page_no) do
+    min(604, page_no + 1)
+  end
+end
