63e12ab176b4a7678d009824e2a4381835dcf6be
Mohamed Aly <mksaga@hey.com>
2022-05-04 17:54:01 -0400

    Fix codex_observer migration

--- priv/repo/migrations/20220504161338_add_codex_observer.exs
+++ priv/repo/migrations/20220504161338_add_codex_observer.exs
@@ -2,7 +2,7 @@ defmodule Mushaf.Repo.Migrations.AddCodexObserver do
   use Ecto.Migration
 
   def change do
-    create table(:codex_observer, primary_key: false) do
+    create table(:codex_observer) do
       add :codex_id, references(:codices), primary_key: true
       add :observer_id, references(:users), primary_key: true
     end
