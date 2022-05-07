778aa23ad4123d11378ed0afcc4fe53f0e6c98f8
Mohamed Aly <mksaga@hey.com>
2022-05-06 20:42:23 -0400

    Define link_observer on CodexObserver

--- lib/mushaf/codex/codex_observer.ex
+++ lib/mushaf/codex/codex_observer.ex
@@ -1,10 +1,25 @@
 defmodule Mushaf.CodexObserver do
   use Ecto.Schema
   import Ecto.Changeset
+  alias Mushaf.Repo
   alias Mushaf.Codex
+  alias Mushaf.Accounts
 
   schema "codex_observer" do
     belongs_to :codex, Mushaf.Codex
     belongs_to :observer, Mushaf.Accounts.User
   end
+
+  def link_observer(
+    codex_observer,
+    %Mushaf.Codex{} = codex,
+    %Mushaf.Accounts.User{} = observer
+  ) do
+    codex_observer
+    |> change()
+    |> put_assoc(:codex, codex)
+    |> put_assoc(:observer, observer)
+    |> Repo.insert()
+  end
+
 end
